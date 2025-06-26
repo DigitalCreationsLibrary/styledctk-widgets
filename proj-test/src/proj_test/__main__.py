f8a1238bbe81a989a1440eb337217ae38afd4883
Amina-Delali <aimosta.official@gmail.com>
2025-06-26 16:06:35 +0100

    removed and example project

--- proj-test/src/proj_test/__main__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-FileCopyrightText: 2025-present DigitalCreationsLibrary <aimosta.official@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-import sys
-
-if __name__ == "__main__":
-    from proj_test.cli import proj_test
-
-    sys.exit(proj_test())
